b0077edfea0d3ecbe0f42db2d0312a0db828ba92
RVESH <rishabhverma.rv2006@gmail.com>
2026-02-03 21:56:59 +0530

    false

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,9 @@
 // src/components/header/index.jsx
 import Header from "./Header";
 import Logo from "./Logo";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar" ;
+
+
 
 export { Logo, Navbar };
 export default Header;
